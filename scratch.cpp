32990790418ec355fce39e0f587c6600ae61b92b
nepaulius <pauliuscern.lt@gmail.com>
2019-03-01 19:56:23 +0200

    v0.4

--- scratch.cpp
+++ scratch.cpp
@@ -1,6 +1,138 @@
 #include "galvininkas.h"
 
+void Rusis(std::vector<Studentai> vektstud,int k)
+{
 
+
+    std::ofstream ne;
+    std::ofstream taip;
+    std::stringstream a;
+    std::string failovardas;
+    std::stringstream a1;
+    std::string failovardas1;
+    a<<k;
+    failovardas= "Moka_" + a.str();
+
+    failovardas += ".txt";
+    taip.open(failovardas.c_str(), std::ios::out);
+    a1<<k;
+    failovardas1= "Nemoka_" + a1.str();
+
+    failovardas1 += ".txt";
+    ne.open(failovardas1.c_str(), std::ios::out);
+
+
+    for(int i=0;i<vektstud.size();i++)
+    {
+        if(vektstud[i].galutinis<5)
+        {
+            ne<<std::left<<std::setw(15)<<vektstud[i].vardas<<std::setw(20)<<std::left<<vektstud[i].pavarde;
+            for(int j=0;j<10;j++)
+            {
+                ne<<std::setw(5)<<std::left<<vektstud[i].balai[j];
+            }
+            ne<<std::setw(7)<<std::right<<vektstud[i].testas<<std::setw(20)<< std::setprecision (2) << std::fixed<<vektstud[i].galutinis<<std::right<<std::setw(15)<< std::setprecision (2) << std::fixed<<vektstud[i].galutinismediana<<std::endl;
+
+        }
+        else
+        {
+            taip<<std::left<<std::setw(10)<<vektstud[i].vardas<<std::setw(20)<<std::left<<vektstud[i].pavarde;
+            for(int j=0;j<10;j++)
+            {
+                taip<<std::setw(5)<<std::left<<vektstud[i].balai[j];
+            }
+            taip<<std::setw(7)<<std::right<<vektstud[i].testas<<std::setw(20)<< std::setprecision (2) << std::fixed<<vektstud[i].galutinis<<std::right<<std::setw(15)<< std::setprecision (2) << std::fixed<<vektstud[i].galutinismediana<<std::endl;
+        }
+
+    }
+    ne.close();
+    taip.close();
+
+}
+void Pirmasfailas(int k)
+{
+    auto start = std::chrono::system_clock::now();
+    Studentai gimt;
+    std::vector<Studentai> stud;
+
+    double laikinvid=0;
+    std::string failovardas;
+    std::ofstream fr;
+
+    std::stringstream a;
+    a<<k;
+    failovardas= "Rez_" + a.str();
+
+    failovardas += ".txt";
+    fr.open(failovardas.c_str(), std::ios::out);
+
+    fr<<std::left<<std::setw(15)<<"Vardas"<<std::setw(30)<<std::left<<"Pavarde";
+    for(int i=1;i<11;i++)
+    {
+        fr<<std::setw(5)<<std::left<<"ND"+std::to_string(i);
+    }
+    fr<<std::setw(12)<<std::right<<"Egzaminas"<<std::setw(35)<<std::right<<"Galutinis vid./ Galutinis med"<<std::endl;
+    fr<<std::endl;
+
+
+    srand (time(NULL));
+    for(int i=0;i<std::round(pow(10,k));i++)
+    {
+
+        gimt.vardas="Vardas"+std::to_string(i);
+
+        gimt.pavarde="Pavarde"+std::to_string(i);
+        for(int j=0;j<10;j++)
+        {
+            int bet=rand() % 10 + 1;
+
+            laikinvid=bet+laikinvid;
+
+
+            gimt.balai.push_back(bet);
+        }
+        gimt.testas=rand() % 10 + 1;
+
+
+
+
+        gimt.galutinis = 0.4 * Vidurkis(laikinvid,10) + 0.6 *gimt.testas;
+        gimt.galutinismediana =0.4 * Mediana(gimt.balai,10) + 0.6 *gimt.testas;
+
+
+
+
+        stud.push_back(gimt);
+
+
+        gimt.balai.clear();
+\
+        laikinvid=0;
+    }
+    auto startrusis = std::chrono::system_clock::now();
+    Rusis(stud, k);
+    auto endrusis = std::chrono::system_clock::now();
+    auto uztruko = endrusis - startrusis;
+    std::cout << k<<" karta rusiuojant uztruko :   "<<uztruko.count()<< std::endl;
+
+    for(int i=0;i<stud.size();i++)
+    {
+        fr<<std::left<<std::setw(15)<<stud[i].vardas<<std::setw(30)<<std::left<<stud[i].pavarde;
+        for(int j=0;j<10;j++)
+        {
+            fr<<std::setw(5)<<std::left<<stud[i].balai[j];
+        }
+        fr<<std::setw(7)<<std::right<<stud[i].testas<<std::setw(20)<< std::setprecision (2) << std::fixed<<stud[i].galutinis<<std::right<<std::setw(15)<< std::setprecision (2) << std::fixed<<stud[i].galutinismediana<<std::endl;
+
+    }
+
+
+    fr.close();
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = end - start;
+    //std::cout << k<<" karta skaiciuojant uztruko :   "<<elapsed.count() << std::endl;
+
+}
 std::string Tik2()
 {
   std::string tikrinimas;
