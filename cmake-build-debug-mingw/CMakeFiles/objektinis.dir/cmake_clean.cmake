f5930ba219f778fc308570f8e75ae34fa6bdc43f
nepaulius <pauliuscern.lt@gmail.com>
2019-03-01 20:00:57 +0200

    v0.4

---
+++ cmake-build-debug-mingw/CMakeFiles/objektinis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objektinis.dir/main.cpp.obj"
+  "../build/objektinis.pdb"
+  "../build/objektinis.exe"
+  "../build/objektinis.exe.manifest"
+  "libobjektinis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objektinis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
